c566df9aa72604d2ae658decc3fb91dce3ec486a
chulei <chu.leilei@vpclub.cn>
2018-09-15 14:51:46 +0800

    a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,9 @@
   <v-dialog>
     <div class="hello">
       <div @click="clickFull()" class="full-screen">全屏</div>
-      <iframe :class="isFull?'full':''" id="ifr" src="http://yun.baiyug.cn/vip/index.php?url=http://www.iqiyi.com/v_19rr96agog.html"></iframe>    
+      <iframe :class="isFull?'full':''" id="ifr" src="http://api.visaok.net/?url=https://www.iqiyi.com/v_19rrd2h7j0.html"></iframe>    
     </div>
+  
   </v-dialog>  
 </template>
 
