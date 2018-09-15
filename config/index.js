9bfa59bc1629ebf88ab72718b5405536e535ecb3
chulei <chu.leilei@vpclub.cn>
2018-09-15 11:41:13 +0800

    a

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/hello',
     proxyTable: {},
 
     // Various Dev Server settings
